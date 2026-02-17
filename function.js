38b6f84f2c2d14442369bec0159d2a26d1d46242
georgewakhu270 <georgewakhu270@gmail.com>
2026-02-17 14:52:08 +0300

    added code for the carousel

--- function.js
+++ function.js
@@ -1,1 +1,34 @@
+class Carousel { }
 
+const next = document.querySelector(".two .carousel .car-btn#next")
+const prev = document.querySelector('.two .carousel .car-btn#prev')
+const slides = document.querySelector('.two .slides')
+let currentIndex = 0
+
+const slide = document.querySelectorAll('.two .slide')
+
+let totalSlides = slide.length
+
+console.log(totalSlides)
+
+const moveSlide = (items, direction) => {
+    const maxIndex = totalSlides - items
+    console.log(maxIndex)
+    currentIndex = currentIndex + direction
+    console.log(`current index ${currentIndex}`)
+    if (currentIndex < 0) {
+        currentIndex = 0
+    } else if(currentIndex > maxIndex) {
+        currentIndex = maxIndex
+    }
+    const itemWidth = 100 / items
+    slides.style.transform = `translateX(-${currentIndex*itemWidth}%)`
+}
+
+next.addEventListener('click', () => {
+    moveSlide(4, 1)
+})
+
+prev.addEventListener('click', () => {
+    moveSlide(4, -1)
+})
